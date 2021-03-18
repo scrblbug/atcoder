6c77546bbe86c6544e254da77ce2ec14f13ef876
scrblbug <scrblbug@gmail.com>
2021-03-18 13:26:48 +0900

    whileの使用を止め、より素直な実装に変更

--- python/abc174_f.py
+++ python/abc174_f.py
@@ -9,9 +9,8 @@
 # 一番楽でギリ間に合う……？
 # 定数倍でもそれなりに削らないと、Python(Pypy)では厳しい。
 
-# 玉の種類を数える＝全部の玉の数から、ダブっている玉の数を引く、と考える。
+# 玉の種類を数える＝最後に現れた玉だけをカウントする、と考える
 # そこで、色 i の玉が前回現れた地点を管理するリストを用意する。
-# また、ダブっている玉の場所を管理するリストも用意する。
 
 # クエリについては右端であらかじめソートしておき、
 # 順次新しい玉を追加していくとともに、クエリへの回答を行う。
@@ -19,18 +18,17 @@
 # 入力例2 [2, 5, 6, 5, 2, 1, 7, 9, 7, 2] の例
 # 今最初の4つの玉 [2, 5, 6, 5] を見ている時、
 # last_appeared = [-1, -1, 0, -1, -1, 3, 2, ...] (-1 は未出現)
-# dubbed = [0, 1, 0, 0, ....]
+# count = [1, 0, 1, 1, ....]
 
 # 次に5つ目の玉を追加して [2, 5, 6, 5, 2] となった時、
 # last_appeared = [-1, -1, 4, -1, -1, 3, 2, ...]
-# dubbed = [1, 1, 0, 0, 0, ...]
+# count = [0, 0, 1, 1, 1, ...]
 # のように更新される
 
 # クエリ問い合わせ(l, r)については、
-# (r - l + 1) - sum(dubbed[l-1:r]) が回答となるが、
-# この sum を素早く計算するため、フェニック木・セグメント木で
-# 管理を行うこととする。が、numpy のコンパイルを使わない環境では
-# セグ木では厳しいかも……？
+# sum(count[l-1:r]) が回答となるが、
+# この sum を素早く計算するため、フェニック木 or セグメント木で
+# 管理を行うこととする。
 
 # ここでは、フェニック木を使用した
 class BIT:
@@ -51,46 +49,46 @@ class BIT:
             result = result + self.tree[pos]
             pos = pos - (pos & -pos)
         return result
-
-    def get_sum_from_to(self, pos_from, pos_to):
-        return self.get_sum_to(pos_to) - self.get_sum_to(pos_from - 1)
+    
+    def get_sum_from_to(self, l, r):
+        return self.get_sum_to(r) - self.get_sum_to(l-1)
 
 def main():
     N, Q = map(int, input().split())
     balls = list(map(int, input().split()))
+    queries = [list(map(int, input().split())) for _ in range(Q)]
 
-    # インデックスを追加つつクエリを取り込み、r でソートする
-    # 地味に定数倍改善のため、内包表記で行っている
-    sort_q = [(i, l-1, r-1) for i, (l, r) in enumerate([list(map(int, input().split())) for _ in range(Q)])]
-    sort_q.sort(key=lambda x:x[2])
+    # r 毎にクエリを整理しておく
+    # ついでに、l, r を 0-indexed に整理
+    # あとからクエリ順に回答を行うので、インデックスも一緒に持っておく
+    sorted_q = [[] for _ in range(N)]
+    for i, (l, r) in enumerate(queries):
+        sorted_q[r-1].append((l-1, i))
 
     # 管理用リスト・フェニック木
     last_appeared = [-1] * (N+1)
-    dubbed = BIT(N)
+    count = BIT(N)
 
-    # 今いくつ目のボールまで追加しているか
-    now = -1
+    # 回答保管用リスト
+    answers = [0] * Q
 
-    result = [0] * Q
+    # ボールを追加しつつ、クエリがあれば処理していく
+    # 当初クエリが現れる r まで while で回してやってみていたが、
+    # while は for に比べて遅いので、全て for で回すのが吉
+    for r in range(N):
+        count.add(r, 1)
 
-    for idx, l, r in sort_q:
-        while now < r:
-            now += 1
-            color = balls[now]
-            # ボールが初出の場合
-            if last_appeared[color] == -1:
-                last_appeared[color] = now
-            # ボールが2個目以降の場合
-            else:
-                prev_pos = last_appeared[color]
-                dubbed.add(prev_pos, 1)
-                last_appeared[color] = now
-        # ダブっているボールの数を引いたものが答え
-        result[idx] = r - l + 1 - dubbed.get_sum_from_to(l, r)
+        # 以前現れている色の玉なら、前の玉をカウントから外す
+        if last_appeared[balls[r]] != -1:
+            count.add(last_appeared[balls[r]], -1)
 
-    # 改めて元のクエリ順に回答を出力
-    for r in result:
-        print(r)
+        last_appeared[balls[r]] = r
+
+        # 該当するクエリがあれば、処理して回答を格納
+        for l, i in sorted_q[r]:
+            answers[i] = count.get_sum_from_to(l, r)
+    
+    for ans in answers:
+        print(ans)
 
 main()
-
