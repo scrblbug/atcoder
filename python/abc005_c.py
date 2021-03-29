8f6d2179fad2b97988fd3fefcd04ecd8082f6ae7
scrblbug <scrblbug@gmail.com>
2021-03-29 16:14:20 +0900

    コメント追加・変数名変更

--- python/abc005_c.py
+++ python/abc005_c.py
@@ -2,6 +2,10 @@
 # https://atcoder.jp/contests/abc005/tasks/abc005_3
 # tag: 高橋君
 
+# 制約も緩いので、素直に書かれている通りのことを
+# 実装していくだけ。
+# ここではインデックスを進める形で解いているが、
+# キューなどを使用すると多少やりやすいかも？
 def main():
     T = int(input())
     N = int(input())
@@ -9,21 +13,30 @@ def main():
     M = int(input())
     customers = list(map(int, input().split()))
 
-    now = 0
+    # たこ焼き用インデックス
+    tkyk_idx = 0
+
+    # 客ごとに見ていく
     for c in customers:
-        if now >= len(takoyakis):
+        # たこ焼きが尽きたらNG
+        if tkyk_idx >= len(takoyakis):
             print('no')
             return
-        if takoyakis[now] > c:
+        # 客の来店に間に合うたこ焼きがなければNG
+        if takoyakis[tkyk_idx] > c:
             print('no')
             return
-        while c - takoyakis[now] > T:
-            now += 1
-            if now >= len(takoyakis):
+        # たこ焼きが古すぎたら、使えるのが出てくるまで廃棄
+        while c - takoyakis[tkyk_idx] > T:
+            tkyk_idx += 1
+            # 客の来店に間に合わないときはNG
+            if tkyk_idx >= len(takoyakis):
                 print('no')
                 return
-        now += 1
+        # 客にたこ焼きを渡す
+        tkyk_idx += 1
     
+    # 客を捌き切ったらOK
     print('yes')
 
 main()
