9926a3416dd64905fe9994575ba55575512f5c5f
scrblbug <scrblbug@gmail.com>
2021-04-02 10:41:26 +0900

    初回コミット

---
+++ python/abc027_b.py
@@ -0,0 +1,44 @@
+# AtCoder Beginner Contest 027 B - 島と橋
+# https://atcoder.jp/contests/abc027/tasks/abc027_b
+# tag: 高橋君
+
+# 左側から順番に、橋をかける必要があるかどうか判定していく。
+
+def main():
+    N = int(input())
+    pops = list(map(int, input().split()))
+
+    total = sum(pops)
+
+    # そもそも人数を均等にできないなら、-1 を返して終了
+    if total % N != 0:
+        print(-1)
+        return
+
+    # 各島あたりの人数を確認しておく
+    each = total // N
+
+    # 今繋げている島の数と、そこにいる人数を管理
+    n_islands = 0
+    g_pop = 0
+
+    result = 0
+    # 左端から島を見ていき、今の状態で各島の人数を
+    # 条件に合うように分配可能かどうかを確認する。
+    # 条件にあっていれば、次の島に橋を架ける必要はないが、
+    # 条件にあってなければ、橋を架ける必要がある。
+    for p in pops:
+        n_islands += 1
+        g_pop += p
+
+        # 今の状態で分配可能なら、橋は架けずに次の島から改めて見ていく
+        if g_pop / n_islands == each:
+            n_islands = 0
+            g_pop = 0
+        # ダメなら、橋を架ける ＝ 次の島の分も加算して考えていく
+        else:
+            result += 1
+    
+    print(result)
+
+main()
---
+++ python/arc080_d.py
@@ -0,0 +1,43 @@
+# AtCoder Regular Contest 080 D - Grid Coloring
+# https://atcoder.jp/contests/arc080/tasks/arc080_b
+# tag: グリッド 特殊構造
+
+# 同じ色なら全て繋がっている必要がある ＝ 繋がってさえいれば形は問わない
+# グリッド全体をひと筆書きできるような経路があれば、
+# 一筆書きの各分割区間は連結しているとみなせるので、
+# そこに各色を求められる数だけ順番に置いていけばいい。
+
+# たぶん一番簡単で例外がないのが、
+# 123
+# 654
+# 789
+# みたいな経路。
+
+# ↑を簡単に作るにはどうしよう……
+# 123
+# 456
+# 789
+# みたいに普通に並べてから、奇数行だけひっくり返せばいいのでは？
+
+def main():
+    H, W = map(int, input().split())
+    N = int(input())
+    n_by_color = list(map(int, input().split()))
+
+    # とりあえずひとつなぎの配列を作成し、後から整形する
+    seq = []
+    for c, n in enumerate(n_by_color, start=1):
+        for i in range(n):
+            seq.append(c)
+
+    # 先程作ったものを、W ごとに H 個に分割しつつ……
+    for h in range(H):
+        row = seq[h*W:h*W+W]
+
+        # 一行おきにひっくり返しながら出力する
+        if h % 2 == 0:
+            print(*row)
+        else:
+            print(*row[::-1])
+
+main()
