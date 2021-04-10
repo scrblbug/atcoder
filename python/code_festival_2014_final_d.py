96e7df62fa8182b4ea43c75bf5e5ff78cbd71e86
scrblbug <scrblbug@gmail.com>
2021-04-10 18:47:48 +0900

    初回コミット

---
+++ python/code_festival_2014_final_d.py
@@ -0,0 +1,12 @@
+# CODE FESTIVAL 2014 決勝 D - パスカルの三角形
+# https://atcoder.jp/contests/code-festival-2014-final/tasks/code_festival_final_d
+# tag:
+
+# 何段か書いてみれば、すぐに気づくと思うが、
+# n 段目の左から 2 個目の数字は n-1 になる (n >= 2)
+
+def main():
+    A = int(input())
+    print(A+1, 2)
+
+main()
