d5e6ec3bed7cf99b8002bbaa6fb14e6752ff7f37
scrblbug <scrblbug@gmail.com>
2021-04-15 10:14:49 +0900

    初回コミット

---
+++ python/abc015_c.py
@@ -0,0 +1,34 @@
+# AtCoder Beginner Contest 015 C - 高橋くんのバグ探し
+# https://atcoder.jp/contests/abc015/tasks/abc015_3
+# tag: 基礎問題 高橋君
+
+# 制約から、回答の組み合わせは高々 5^5 = 3125 通りなので
+# 全探索を行う。
+
+def main():
+    N, K = map(int, input().split())
+    questions = [list(map(int, input().split())) for _ in range(N)]
+
+    # 回答により現れる数値を保存
+    patterns = [0]
+
+    # 質問毎に得られる数値のパターンを更新していく
+    for q in questions:
+        # 更新用の新規リスト
+        new_patterns = []
+
+        # 前回の数値と今回の回答をすべて組み合わせ、
+        # xor したものを新しいパターンとする
+        for p in patterns:
+            for choice in q:
+                new_patterns.append(p^choice)
+
+        # 更新して次の質問へ
+        patterns = new_patterns
+
+    if 0 in patterns:
+        print('Found')
+    else:
+        print('Nothing')
+
+main()
