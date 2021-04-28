26109b6bd53f3e8e3e359affe9c7b2a563c517e8
scrblbug <scrblbug@gmail.com>
2021-04-28 23:29:00 +0900

    コメント修正

--- python/code_festival_2014_final_d.py
+++ python/code_festival_2014_final_d.py
@@ -1,9 +1,10 @@
 # CODE FESTIVAL 2014 決勝 D - パスカルの三角形
 # https://atcoder.jp/contests/code-festival-2014-final/tasks/code_festival_final_d
-# tag:
+# tag: 二項係数 基礎問題
 
 # 何段か書いてみれば、すぐに気づくと思うが、
-# n 段目の左から 2 個目の数字は n-1 になる (n >= 2)
+# n 段目の左から（もしくは右から） 2 個目の数字は
+# n-1 になる (n >= 2)
 
 def main():
     A = int(input())
