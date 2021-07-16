8d2eca710210f7e0d45a4d8d5a088d05a5bd0b30
scrblbug <scrblbug@gmail.com>
2021-07-16 16:08:00 +0900

    コメント・タグ修正

--- python/abc185_f.py
+++ python/abc185_f.py
@@ -1,6 +1,6 @@
 # AtCoder Beginner Contest 185 F - Range Xor Query
 # https://atcoder.jp/contests/abc185/tasks/abc185_f
-# tag: ビット操作 範囲参照 BIT セグメント木 基礎問題 典型問題
+# tag: ビット操作 範囲参照 フェニック木 セグメント木 基礎問題 典型問題
 
 # xor はビットの反転操作。
 # 仮に Ti = 1 の時の操作、すなわち値の書き換えが
@@ -9,7 +9,8 @@
 
 # 逆に言うと、値の書き換えがある場合には、単純な累積和で
 # 解くことができない。
-# Binary Indexed Tree (BIT)、あるいはセグメント木を
+# フェニック木(Binary Indexed Tree)、
+# あるいはセグメント木を
 # 用いる必要がある。
 
 # ここでは、実装の簡単な BIT で解いてみる。
@@ -26,6 +27,7 @@ def main():
             result ^= bitree[x]
             x -= x & -x
         return result
+
     def op_xor(x, y):
         while x <= N:
             bitree[x] ^= y
