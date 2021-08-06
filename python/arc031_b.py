3f4343cf1813b20b85b4279a89f1b8455651d235
scrblbug <scrblbug@gmail.com>
2021-08-06 09:19:02 +0900

    連結判定を関数に変更

--- python/arc031_b.py
+++ python/arc031_b.py
@@ -6,63 +6,64 @@
 # 確認していく。
 
 def main():
-    field = [[c=='o' for c in input()] for _ in range(10)]
+    field = [[c for c in input()] for _ in range(10)]
+    
+    # 与えられた地図を元に、陸地が全てつながっているか
+    # どうかを確認する関数を作成しておく。
+    def land_connectedp(field):
+        moves = ((-1, 0), (1, 0), (0, -1), (0, 1))
+        connection = 0
+        visited = [[False] * 10 for _ in range(10)]
 
-    # 全部陸地なら YES
-    if all(v==True for r in range(10) for v in field[r]):
+        # 探索開始地点をすべての場所から試す
+        for sx in range(10):
+            for sy in range(10):
+                # 訪問済み、もしくは海ならスキップする
+                if visited[sy][sx] or field[sy][sx] == 'x':
+                    continue
+                
+                # 陸地が見つかったら、連結成分数のカウントを増やしつつ、
+                # 繋がっている陸地を全て訪問済みにする。
+                connection += 1
+                queue = [(sx, sy)]
+                visited[sy][sx] = True
+                while queue:
+                    x, y = queue.pop()
+                    for dx, dy in moves:
+                        tx, ty = x + dx, y + dy
+                        if not (0 <= tx < 10 and 0 <= ty < 10):
+                            continue
+                        if visited[ty][tx] == False and field[ty][tx] == 'o':
+                            visited[ty][tx] = True
+                            queue.append((tx, ty))
+
+        # 連結成分数が 1 なら、陸地は全て繋がっている。
+        return connection == 1
+    
+    # 最初から繋がっていれば YES
+    if land_connectedp(field):
         print('YES')
         return
-
-    moves = ((-1, 0), (1, 0), (0, -1), (0, 1))
-
-    # ひとマスずつ埋め立て地点を探索していく
-    for fill_y in range(10):
-        for fill_x in range(10):
-            if field[fill_y][fill_x] == True:
+    
+    # 埋立地の場所を全探索する。
+    for fx in range(10):
+        for fy in range(10):
+            # 陸ならスキップ。
+            if field[fy][fx] == 'o':
                 continue
-            # 埋め立てる
-            field[fill_y][fill_x] = True
-
-            # 連結成分数を求める
-            connect_n = 0
-            visited = [[False] * 10 for _ in range(10)]
-
-            # スタート位置を全探索 
-            for y in range(10):
-                for x in range(10):
-                    # 訪問済みならスキップ
-                    if visited[y][x]:
-                        continue
-
-                    # 陸地でなければスキップ
-                    if field[y][x] == False:
-                        continue
-
-                    # 連結成分に +1 し、繋がっている陸地を訪問済みにする
-                    visited[y][x] = True
-                    connect_n += 1
-                    queue = [(x, y)]
-                    while queue:
-                        x_now, y_now = queue.pop()
-                        for dx, dy in moves:
-                            x_nxt, y_nxt = x_now + dx, y_now + dy
-                            if not (0 <= x_nxt < 10 and 0 <= y_nxt < 10):
-                                continue
-                            if visited[y_nxt][x_nxt]:
-                                continue
-                            if field[y_nxt][x_nxt] == True:
-                                visited[y_nxt][x_nxt] = True
-                                queue.append((x_nxt, y_nxt))
-
-            # 連結成分数が 1 なら YES
-            if connect_n == 1:
+            
+            # 海なら埋め立て、つながるかどうか確認する。
+            field[fy][fx] = 'o'
+            if land_connectedp(field):
                 print('YES')
                 return
-
-            # 埋め立てたものを元に戻す
-            field[fill_y][fill_x] = False
-
-    # 連結成分数が 1 になるものが見つからなければ、NO
+            field[fy][fx] = 'x'
+    
+    # 条件に合う埋立地が見つからなければ、NO
     print('NO')
 
 main()
+
+
+
+
