dad9e11edf869c943b090b1356e99a001b25dfd6
scrblbug <scrblbug@gmail.com>
2021-09-13 16:01:10 +0900

    初回コミット

---
+++ python/abc042_c.py
@@ -0,0 +1,20 @@
+# AtCoder Beginner Contest 042 C - こだわり物いろはちゃん
+# https://atcoder.jp/contests/abc042/tasks/arc058_a
+# tag: 愚直 考察
+
+# N 円から順に 1 円ずつ増やしながら、条件に合った金額か
+# どうか確かめる。
+# せいぜい 10N までいく間に見つかるので、十分間に合う。
+
+def main():
+    N, K = map(int, input().split())
+    dislikes = input().split()
+
+    result = N
+    while True:
+        if all(d not in str(result) for d in dislikes):
+            print(result)
+            return
+        result += 1
+
+main()
